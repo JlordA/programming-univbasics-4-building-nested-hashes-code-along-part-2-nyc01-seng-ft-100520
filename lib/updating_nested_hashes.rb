904145fe18a9df77e35796546e8867d8e27f7b6c
placid-tier-4550 <james.ardery@gmail.com>
2020-09-22 18:16:37 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -24,7 +24,7 @@ def update_status
   binding.pry
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
-  if epic_tragedy[:montague][:hero][:status] || epic_tragedy[:capulet][:heroine][:status] == "alive" do
+  if epic_tragedy[:montague][:hero][:status] || epic_tragedy[:capulet][:heroine][:status] == "alive"
     epic_tragedy[:montague][:hero][:status] = "dead"
   end
 
